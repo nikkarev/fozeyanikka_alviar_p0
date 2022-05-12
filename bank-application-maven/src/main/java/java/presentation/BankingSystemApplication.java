b0545c8bec963909171297f5f7d818b02bffdf5a
nikkarev <fozeya-nikka397@revature.net>
2022-05-11 22:25:29 -0400

    first commit

---
+++ bank-application-maven/src/main/java/java/presentation/BankingSystemApplication.java
@@ -0,0 +1,11 @@
+package presentation;
+
+import java.util.Scanner;
+
+public class BankingSystemApplication {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in); // may change the var name
+		
+	}
+
+}
